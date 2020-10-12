10bc28de884b653f7ccf250f522106f2a1516b8c
T Lim <timlimhk@gmail.com>
2020-10-12 15:03:00 +1100

    update set-env.sh

--- codefresh/set-env.sh
+++ codefresh/set-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git_commit_sha="$(git rev-parse --short HEAD 2>/dev/null)"
-if [ $git_commit_sha=="" ]
+if [[ $git_commit_sha == "" ]]
 then
-     git_commit_sha=="undefinedbyCF"
+     git_commit_sha="undefinedbyCF"
 fi
 
 export git_commit_sha
