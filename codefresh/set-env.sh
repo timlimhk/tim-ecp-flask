6fc01c1bd0ec0821955b08dc5e99005c94593f25
T Lim <timlimhk@gmail.com>
2020-10-12 15:00:12 +1100

    added set-env.sh

---
+++ codefresh/set-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git_commit_sha="$(git rev-parse --short HEAD 2>/dev/null)"
+if [ $git_commit_sha=="" ]
+then
+     git_commit_sha=="undefinedbyCF"
+fi
+
+export git_commit_sha
+export log_level=INFO
+export version=1.0.2
+export service_port=5000
+
