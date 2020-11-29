eb0cfe734a02014cb39b3786b30485863c2140da
T Lim <timlimhk@gmail.com>
2020-11-29 20:03:34 +1100

    Added env variables

--- codefresh/set-env.sh
+++ codefresh/set-env.sh
@@ -9,4 +9,8 @@ export git_commit_sha
 export log_level=INFO
 export version=1.0.2
 export service_port=5000
+export KUBE_CONTEXT=devops-catalog@doc-cf-gke
+export CHART_REPO_URL=cm://h.cfcr.io/elitecom/default
+export NAMESPACE=default
+
 
